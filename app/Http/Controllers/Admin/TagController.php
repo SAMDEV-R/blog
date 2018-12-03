02d0bdc937a9c60952805bde81a7b67ccbff4501
SAM_DEV <sandresmartinezr@gmail.com>
2018-12-03 13:03:46 -0500

    Create request to validate Tags

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TagStoreRequest;
+use App\Http\Requests\TagUpdateRequest;
 use App\Tag;
 
 class TagController extends Controller
@@ -39,7 +41,7 @@ class TagController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TagStoreRequest $request)
     {
         $tag = Tag::create($request->all());
 
@@ -78,7 +80,7 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
         $tag = Tag::findOrFail($id)->update();
         return redirect()->route('tags.edit',$tag->id)->with('info','Etiquete actualizada con éxito');
